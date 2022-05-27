ad2b151e009a94107a32a91bc6f8741139663e12
Gabriel <gabrielvitor.frasao@gmail.com>
2022-05-27 19:42:42 -0300

    img names fixed

--- public/images/index.ts
+++ public/images/index.ts
@@ -1,9 +1,9 @@
 export * as answer from "./answer.svg";
 export * as check from "./check.svg";
 export * as copy from "./copy.svg";
-export * as delete from "./delete.svg";
-export * as emptyQuestions from "./emptyQuestions.svg";
-export * as googleIcon from "./googleIcon.svg";
+export * as deleteImg from "./delete.svg";
+export * as emptyQuestions from "./empty-questions.svg";
+export * as googleIcon from "./google-icon.svg";
 export * as illustration from "./illustration.svg";
 export * as like from "./like.svg";
 export * as logo from "./logo.svg";
