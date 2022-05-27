e24cefe9479032756885cafa9ec649b6fcb458cc
Gabriel <gabrielvitor.frasao@gmail.com>
2022-05-27 19:33:44 -0300

    template layout for home and new room pages

---
+++ components/layout/Template/template.tsx
@@ -0,0 +1,20 @@
+import Head from "next/head";
+import { TemplateProps } from "@/types";
+
+export const Template = ({ children }: TemplateProps) => {
+  return (
+    <div className="min-h-screen bg-primary-100 text-primary-900">
+      <Head>
+        <meta httpEquiv="Content-Type" content="text/html;charset=UTF-8" />
+        <title>Create Next App</title>
+        <meta
+          name="description"
+          content="Q&amp;A rooms for content creators."
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      {children}
+    </div>
+  );
+};
