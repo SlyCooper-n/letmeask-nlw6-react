b0e777ae123363437568b1ea5d9adea9a30e28ec
Gabriel <gabrielvitor.frasao@gmail.com>
2022-05-27 20:52:47 -0300

    sign in function added

--- contexts/authContext.tsx
+++ contexts/authContext.tsx
@@ -1,9 +1,59 @@
-import React, { ReactNode, useContext } from "react";
+import { app, auth } from "@/firebase";
+import { AuthContextProps, UserType } from "@/types";
+import React, { ReactNode, useEffect, useState } from "react";
 
-export const AuthContext = React.createContext({} as any);
+export const AuthContext = React.createContext({} as AuthContextProps);
 
 export const AuthProvider = (props: { children: ReactNode | ReactNode[] }) => {
+  const [user, setUser] = useState<UserType>();
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged(auth.getAuth(app), (user) => {
+      if (user) {
+        const { displayName, photoURL, uid } = user;
+
+        if (!displayName || !photoURL) {
+          throw new Error("Missing information from Google account.");
+        }
+
+        setUser({
+          id: uid,
+          name: displayName,
+          avatar: photoURL,
+        });
+      } else {
+        setUser(undefined);
+      }
+    });
+
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
+  async function signInWithGoogle() {
+    const provider = new auth.GoogleAuthProvider();
+
+    const result = await auth.signInWithPopup(auth.getAuth(app), provider);
+
+    if (result.user) {
+      const { displayName, photoURL, uid } = result.user;
+
+      if (!displayName || !photoURL) {
+        throw new Error("Missing information from Google account.");
+      }
+
+      setUser({
+        id: uid,
+        name: displayName,
+        avatar: photoURL,
+      });
+    }
+  }
+
   return (
-    <AuthContext.Provider value={{}}>{props.children}</AuthContext.Provider>
+    <AuthContext.Provider value={{ user, signInWithGoogle }}>
+      {props.children}
+    </AuthContext.Provider>
   );
 };
