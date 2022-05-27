cb767d2209106bccd9bfae3522f3c6011d845baa
Gabriel <gabrielvitor.frasao@gmail.com>
2022-05-27 19:31:44 -0300

    colors and font family extended

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,25 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          100: "#f8f8f8",
+          200: "#a8a8b3",
+          500: "#835afd",
+          900: "#29292e",
+        },
+      },
+      fontFamily: {
+        primary: ["Roboto"],
+        secondary: ["Poppins"],
+      },
+    },
   },
   plugins: [],
 };
