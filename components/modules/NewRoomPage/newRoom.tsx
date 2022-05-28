0564481b2e7677f15142fa4a6fa86e2857dfe79b
Gabriel <gabrielvitor.frasao@gmail.com>
2022-05-28 12:47:00 -0300

    function to create room

--- components/modules/NewRoomPage/newRoom.tsx
+++ components/modules/NewRoomPage/newRoom.tsx
@@ -2,8 +2,35 @@ import Image from "next/image";
 import { Button } from "components/widgets/Button";
 import Link from "next/link";
 import { logo } from "@/images";
+import { FormEvent, useRef, useState } from "react";
+import { app, db } from "@/firebase";
+import { useAuth } from "hooks/useAuth";
+import { useRouter } from "next/router";
 
 export const NewRoomPage = () => {
+  const { user } = useAuth();
+  const router = useRouter();
+  const [newRoom, setNewRoom] = useState("");
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  async function handleCreateRoom(event: FormEvent) {
+    event.preventDefault();
+
+    if (newRoom.trim() === "") {
+      setNewRoom("");
+      inputRef.current?.focus();
+      return;
+    }
+
+    const roomRef = db.ref(db.getDatabase(app), "rooms");
+    const firebaseRoom = await db.push(roomRef, {
+      title: newRoom,
+      authorId: user?.id,
+    });
+
+    router.push(`/rooms/${firebaseRoom.key}`);
+  }
+
   return (
     <main className="px-8 flex-[8] flex justify-center items-center">
       <div className="w-full max-w-xs flex flex-col text-center">
@@ -15,10 +42,13 @@ export const NewRoomPage = () => {
           Create a new room
         </h2>
 
-        <form>
+        <form onSubmit={handleCreateRoom}>
           <input
+            ref={inputRef}
             type="text"
-            placeholder="Type the room code"
+            placeholder="Type the room name"
+            value={newRoom}
+            onChange={(event) => setNewRoom(event.target.value)}
             className="w-full h-12 px-4 bg-white border border-primary-200 rounded-lg"
           />
 
