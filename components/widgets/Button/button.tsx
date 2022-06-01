136e97e485ef06f9e6cb1221a3a2786604cfbf20
Gabriel <gabrielvitor.frasao@gmail.com>
2022-06-01 14:54:50 -0300

    height and padding fixed

--- components/widgets/Button/button.tsx
+++ components/widgets/Button/button.tsx
@@ -4,7 +4,7 @@ export const Button = (props: ButtonProps) => {
   return (
     <button
       {...props}
-      className={`flex justify-center items-center bg-primary-500 text-white font-medium rounded-lg transition-all duration-200 hover:brightness-90 ${props.mystyle}`}
+      className={`h-12 px-8 flex justify-center items-center bg-primary-500 text-white font-medium rounded-lg transition-all duration-200 hover:brightness-90 ${props.mystyle}`}
     >
       {props.children}
     </button>
