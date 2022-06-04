554f9462ff75d3a917a1be5f897e20085c637251
Gabriel <gabrielvitor.frasao@gmail.com>
2022-06-04 16:15:28 -0300

    outlined button created

--- components/widgets/Button/button.tsx
+++ components/widgets/Button/button.tsx
@@ -1,6 +1,20 @@
 import { ButtonProps } from "@/types";
 
-export const Button = (props: ButtonProps) => {
+export const Button = ({
+  outlined,
+  ...props
+}: ButtonProps & { outlined?: boolean }) => {
+  if (outlined) {
+    return (
+      <button
+        {...props}
+        className={`h-12 px-8 flex justify-center items-center border border-primary-500 text-primary-500 font-medium rounded-lg transition-all duration-200 hover:brightness-90 ${props.mystyle}`}
+      >
+        {props.children}
+      </button>
+    );
+  }
+
   return (
     <button
       {...props}
