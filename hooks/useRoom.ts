dc1ac58f9e70c1e1990b9eee391028a2fda9151e
Gabriel <gabrielvitor.frasao@gmail.com>
2022-06-04 19:16:54 -0300

    parsedQuestions keys updated

--- hooks/useRoom.ts
+++ hooks/useRoom.ts
@@ -1,8 +1,10 @@
 import { app, db } from "@/firebase";
 import { DatabaseRoomProps, QuestionType } from "@/types";
 import { useEffect, useState } from "react";
+import { useAuth } from "./useAuth";
 
 export function useRoom(roomID: string) {
+  const { user } = useAuth();
   const [authorID, setAuthorID] = useState("");
   const [title, setTitle] = useState("");
   const [questions, setQuestions] = useState<QuestionType[]>([]);
@@ -14,10 +16,20 @@ export function useRoom(roomID: string) {
       const databaseRoom = snapshot.val() as DatabaseRoomProps;
       const roomQuestions = databaseRoom?.questions ?? {};
 
-      const parsedQuestions = Object.entries(roomQuestions).map(
+      const parsedQuestions: QuestionType[] = Object.entries(roomQuestions).map(
         ([key, value]) => ({
           id: key,
-          ...value,
+          author: {
+            avatar: value.author.avatar,
+            name: value.author.name,
+          },
+          content: value.content,
+          isAnswered: value.isAnswered,
+          isHighlighted: value.isHighlighted,
+          likesCount: Object.values(value.likes ?? {}).length,
+          likeId: Object.entries(value.likes ?? {}).find(
+            ([, like]) => like?.authorId === user?.id
+          )?.[0],
         })
       );
 
@@ -25,7 +37,11 @@ export function useRoom(roomID: string) {
       setTitle(databaseRoom?.title);
       setQuestions(parsedQuestions);
     });
-  }, [roomID]);
+
+    return () => {
+      db.off(roomRef, "value");
+    };
+  }, [roomID, user]);
 
   return { authorID, title, questions };
 }
