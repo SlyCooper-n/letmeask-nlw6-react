0d3b3e28cb644ee08da7bc36479bfcf61156aa65
Gabriel <gabrielvitor.frasao@gmail.com>
2022-06-04 16:21:35 -0300

    func to get room data replaced by useRoom hook

--- pages/rooms/[roomID]/index.tsx
+++ pages/rooms/[roomID]/index.tsx
@@ -1,42 +1,22 @@
 import { app, db } from "@/firebase";
 import { logo } from "@/images";
-import { DatabaseRoomProps, QuestionType } from "@/types";
-import { Template } from "@components/layout";
 import { Question } from "@components/modules";
 import { Button, RoomCode } from "@components/widgets";
 import { useAuth } from "hooks/useAuth";
+import { useRoom } from "hooks/useRoom";
+import { NextPage } from "next";
 import Image from "next/image";
 import { useRouter } from "next/router";
-import { FormEvent, useEffect, useRef, useState } from "react";
+import { FormEvent, useRef, useState } from "react";
 import toast, { Toaster } from "react-hot-toast";
 
-const RoomPage = () => {
+const RoomPage: NextPage = () => {
   const { user } = useAuth();
   const router = useRouter();
   const { roomID } = router.query;
   const [newQuestion, setNewQuestion] = useState("");
   const textareaRef = useRef<HTMLTextAreaElement>(null);
-  const [title, setTitle] = useState("");
-  const [questions, setQuestions] = useState<QuestionType[]>([]);
-
-  useEffect(() => {
-    const roomRef = db.ref(db.getDatabase(app), `rooms/${roomID}`);
-
-    db.onValue(roomRef, (snapshot) => {
-      const databaseRoom = snapshot.val() as DatabaseRoomProps;
-      const roomQuestions = databaseRoom?.questions ?? {};
-
-      const parsedQuestions = Object.entries(roomQuestions).map(
-        ([key, value]) => ({
-          id: key,
-          ...value,
-        })
-      );
-
-      setTitle(databaseRoom?.title);
-      setQuestions(parsedQuestions);
-    });
-  }, [roomID]);
+  const { title, questions } = useRoom(roomID as string);
 
   async function handleSendQuestion(event: FormEvent) {
     event.preventDefault();
@@ -69,7 +49,7 @@ const RoomPage = () => {
   }
 
   return (
-    <Template>
+    <>
       <header className="p-6 border-b border-[#e2e2e2]">
         <div className="max-w-6xl mx-auto flex flex-col sm:flex-row sm:justify-between sm:items-center">
           <Image src={logo} alt="Letmeask" className="max-h-11" />
@@ -130,11 +110,25 @@ const RoomPage = () => {
 
         <section className="mt-8">
           {questions.map((question) => (
-            <Question key={question.id} {...question} />
+            <Question key={question.id} {...question}>
+              <button>
+                <svg width="24" height="24" viewBox="0 0 24 24" fill="none">
+                  <path
+                    d="M7 22H4C3.46957 22 2.96086 21.7893 2.58579 21.4142C2.21071 21.0391 2 20.5304 2 20V13C2 12.4696 2.21071 11.9609 2.58579 11.5858C2.96086 11.2107 3.46957 11 4 11H7M14 9V5C14 4.20435 13.6839 3.44129 13.1213 2.87868C12.5587 2.31607 11.7956 2 11 2L7 11V22H18.28C18.7623 22.0055 19.2304 21.8364 19.5979 21.524C19.9654 21.2116 20.2077 20.7769 20.28 20.3L21.66 11.3C21.7035 11.0134 21.6842 10.7207 21.6033 10.4423C21.5225 10.1638 21.3821 9.90629 21.1919 9.68751C21.0016 9.46873 20.7661 9.29393 20.5016 9.17522C20.2371 9.0565 19.9499 8.99672 19.66 9H14Z"
+                    stroke="#737380"
+                    strokeWidth="1.5"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  />
+                </svg>
+              </button>
+            </Question>
           ))}
         </section>
       </main>
-    </Template>
+
+      <Toaster />
+    </>
   );
 };
 
