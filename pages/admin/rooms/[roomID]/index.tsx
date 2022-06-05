3834c2a03f052dfc2e519fd60b971b241dcebc1d
Gabriel <gabrielvitor.frasao@gmail.com>
2022-06-05 16:09:36 -0300

    func to send question removed and admin guard added

--- pages/admin/rooms/[roomID]/index.tsx
+++ pages/admin/rooms/[roomID]/index.tsx
@@ -2,52 +2,19 @@ import { app, db } from "@/firebase";
 import { logo } from "@/images";
 import { Question } from "@components/modules";
 import { Button, RoomCode } from "@components/widgets";
-// import { Admin } from "components/guards";
+import { Admin } from "components/guards";
 import { useAuth } from "hooks/useAuth";
 import { useRoom } from "hooks/useRoom";
 import { NextPage } from "next";
 import Image from "next/image";
 import { useRouter } from "next/router";
-import { FormEvent, useRef, useState } from "react";
 import toast, { Toaster } from "react-hot-toast";
 
 const AdminRoomPage: NextPage = () => {
   const { user } = useAuth();
   const router = useRouter();
   const { roomID } = router.query;
-  const [newQuestion, setNewQuestion] = useState("");
-  const textareaRef = useRef<HTMLTextAreaElement>(null);
-  const { title, questions } = useRoom(roomID as string);
-
-  async function handleSendQuestion(event: FormEvent) {
-    event.preventDefault();
-
-    if (newQuestion.trim() === "") {
-      textareaRef.current?.focus();
-      return;
-    }
-
-    if (!user) {
-      throw new Error("User not logged in");
-    }
-
-    const question = {
-      content: newQuestion,
-      author: {
-        name: user.name,
-        avatar: user.avatar,
-      },
-      isHighlighted: false,
-      isAnswered: false,
-    };
-
-    setNewQuestion("");
-    await db.push(
-      db.ref(db.getDatabase(app), `rooms/${roomID}/questions`),
-      question
-    );
-    toast.success("Question sent successfully");
-  }
+  const { authorID, title, questions, loading } = useRoom(roomID as string);
 
   async function handleCloseRoom() {
     await db.update(db.ref(db.getDatabase(app), `rooms/${roomID}`), {
@@ -58,50 +25,95 @@ const AdminRoomPage: NextPage = () => {
     router.push("/");
   }
 
+  if (loading) {
+    return (
+      <div className="flex flex-col items-center justify-center h-screen">
+        <div className="w-36 h-36 flex justify-center items-center bg-gradient-to-br from-primary-500 to-pink-400 rounded-full animate-spin">
+          <div className="w-4/5 aspect-square bg-white rounded-full"></div>
+        </div>
+      </div>
+    );
+  }
+
   return (
-    <>
-      {/* 
-      // userID={user?.id}
-      // roomAuthor={authorID as string}
-      // roomID={roomID as string}
-    > */}
-      <header className="p-6 border-b border-[#e2e2e2]">
-        <div className="max-w-6xl mx-auto flex flex-col sm:flex-row sm:justify-between sm:items-center">
-          <Image src={logo} alt="Letmeask" className="max-h-11" />
-
-          <div className="flex justify-center items-center gap-4">
-            <RoomCode code={roomID as string} />
-
-            <Button outlined onClick={handleCloseRoom}>
-              Close room
-            </Button>
+    <Admin
+      userID={user?.id}
+      roomAuthor={authorID as string}
+      roomID={roomID as string}
+    >
+      <div className="min-h-screen">
+        <header className="p-6 border-b border-[#e2e2e2]">
+          <div className="max-w-6xl mx-auto flex flex-col sm:flex-row sm:justify-between sm:items-center">
+            <Image src={logo} alt="Letmeask" className="max-h-11" />
+            <div className="flex flex-col md:flex-row md:justify-center md:items-center gap-4">
+              <RoomCode code={roomID as string} />
+              <Button outlined onClick={handleCloseRoom}>
+                Close room
+              </Button>
+            </div>
           </div>
-        </div>
-      </header>
+        </header>
 
-      <main className="max-w-[800px] mx-auto">
-        <div className="mx-6 my-8 sm:flex sm:items-center">
-          <h1 className="text-2xl text-primary-900 font-secondary font-medium">
-            Room {title}
-          </h1>
+        <main className="max-w-[800px] mx-auto px-1">
+          <div className="mx-6 my-8 sm:flex sm:items-center">
+            <h1 className="text-3xl text-primary-900 font-secondary font-medium">
+              Room {title}
+            </h1>
 
-          {questions.length > 0 && (
-            <span className="mt-4 sm:mt-0 sm:ml-4 px-4 py-2 inline-block sm:inline bg-[#e559f9] text-white text-sm font-medium rounded-full">
-              {questions.length}{" "}
-              {questions.length === 1 ? "question" : "questions"}
-            </span>
+            {questions.length > 0 && (
+              <span className="mt-4 sm:mt-0 sm:ml-4 px-4 py-2 inline-block sm:inline bg-[#e559f9] text-white text-sm font-medium rounded-full">
+                {questions.length}{" "}
+                {questions.length === 1 ? "question" : "questions"}
+              </span>
+            )}
+          </div>
+
+          {questions.some(
+            (question) => question.isHighlighted && !question.isAnswered
+          ) && (
+            <section>
+              <h2 className="mx-6 mb-4 text-xl">Highlighted question</h2>
+
+              {questions
+                .filter(
+                  (question) => question.isHighlighted && !question.isAnswered
+                )
+                .map((question) => (
+                  <Question key={question.id} type="admin" {...question} />
+                ))}
+            </section>
           )}
-        </div>
 
-        <section className="mt-8">
-          {questions.map((question) => (
-            <Question key={question.id} type="admin" {...question} />
-          ))}
-        </section>
-      </main>
+          <hr className="my-10 brightness-90" />
 
-      <Toaster />
-    </>
+          <section>
+            {questions
+              .filter(
+                (question) => !question.isHighlighted && !question.isAnswered
+              )
+              .map((question) => (
+                <Question key={question.id} type="admin" {...question} />
+              ))}
+          </section>
+
+          <hr className="my-10 brightness-90" />
+
+          {questions.some((question) => question.isAnswered) && (
+            <section>
+              <h2 className="mx-6 mb-4 text-xl">Answered questions</h2>
+
+              {questions
+                .filter((question) => question.isAnswered)
+                .map((question) => (
+                  <Question key={question.id} type="admin" {...question} />
+                ))}
+            </section>
+          )}
+        </main>
+
+        <Toaster />
+      </div>
+    </Admin>
   );
 };
 
