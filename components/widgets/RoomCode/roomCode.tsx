1bc0485189e60a018d3a26c4a4dc3b8524b7d897
Gabriel <gabrielvitor.frasao@gmail.com>
2022-06-05 16:08:19 -0300

    fixed width on mobile

--- components/widgets/RoomCode/roomCode.tsx
+++ components/widgets/RoomCode/roomCode.tsx
@@ -15,7 +15,7 @@ export const RoomCode = (props: RoomCodeProps) => {
       <button
         type="button"
         onClick={handleCopyToClipboard}
-        className="w-fit h-12 mx-auto sm:mx-0 mt-8 sm:mt-0 flex bg-white border border-primary-500 rounded-lg overflow-hidden font-primary"
+        className="w-full md:w-fit h-12 mx-auto sm:mx-0 mt-8 sm:mt-0 flex bg-white border border-primary-500 rounded-lg overflow-hidden font-primary"
       >
         <div className="h-full px-3 flex justify-center items-center bg-primary-500">
           <Image src={copy} alt="Copy room code" />
