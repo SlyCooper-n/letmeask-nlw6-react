b7ef6437837b86649d12d3664f53c591dbc30e9a
Gabriel <gabrielvitor.frasao@gmail.com>
2022-07-10 17:22:04 -0300

    minor changes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
-import "../styles/globals.css";
-import type { AppProps } from "next/app";
 import { AuthProvider } from "@/contexts";
 import { Template } from "@components/layout";
+import type { AppProps } from "next/app";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
