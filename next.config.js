063e8f1c1e5d61cf8ea5f3bf0398f52bc9721019
Gabriel <gabrielvitor.frasao@gmail.com>
2022-07-21 16:58:47 -0300

    Revert "chore: only one export with all config"
    This reverts commit 02e9b32a02b7261a813cb84c59a07af4f4047f5c.

--- next.config.js
+++ next.config.js
@@ -1,18 +1,21 @@
 /** @type {import('next').NextConfig} */
-
-// PWA config and import in Next.js
-const withPWA = require("next-pwa");
-const runtimeCaching = require("next-pwa/cache");
-
-module.exports = withPWA({
+const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ["lh3.googleusercontent.com"],
   },
+};
+
+module.exports = nextConfig;
+
+// PWA config and import in Next.js
+
+const withPWA = require("next-pwa");
+const runtimeCaching = require("next-pwa/cache");
+
+module.exports = withPWA({
   pwa: {
     dest: "public",
-    disable: process.env.NODE_ENV === "development",
-    customWorkerDir: "core/worker",
     runtimeCaching,
   },
 });
