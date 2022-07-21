fffbc58387eece1f68d935daa524a1b01528c0e2
Gabriel <gabrielvitor.frasao@gmail.com>
2022-07-21 17:32:14 -0300

    chore: small pwa config

--- next.config.js
+++ next.config.js
@@ -1,19 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    domains: ["lh3.googleusercontent.com"],
-  },
-};
-
-module.exports = nextConfig;
 
 // PWA config and import in Next.js
-
 const withPWA = require("next-pwa");
 const runtimeCaching = require("next-pwa/cache");
 
 module.exports = withPWA({
+  reactStrictMode: true,
+  images: {
+    domains: ["lh3.googleusercontent.com"],
+  },
   pwa: {
     dest: "public",
     runtimeCaching,
